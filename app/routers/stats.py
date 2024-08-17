790e8b7d2e9642010989badf1cc3616ad864fc7d
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2024-08-17 16:06:37 -0700

    fix-stats-name

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -33,20 +33,17 @@ async def get_player_stats(player_id: int):
 
 @router.get("/name/{full_name}")
 async def get_player_stats(full_name: str):
-    
-    try:
-        conn = sqlitecloud.connect(get_db_url())
-        cursor = conn.execute("SELECT * FROM players WHERE full_name = ?", (full_name,))
-        result = cursor.fetchone()
+  conn = sqlitecloud.connect(get_db_url())
+  cursor = conn.execute("SELECT * FROM players WHERE full_name = ?", (full_name,))
+  result = cursor.fetchone()
 
-        if result is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
-        return {
-          "player_id": result[0],
-          "full_name": result[1],
-          "stats_url": result[2],
-        }
-    except sqlitecloud.Error as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database error")
-    finally:
-        conn.close()
+  if result is None:
+      conn.close()
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
+  
+  conn.close()
+  return {
+    "player_id": result[0],
+    "full_name": result[1],
+    "stats_url": result[2],
+  }  
