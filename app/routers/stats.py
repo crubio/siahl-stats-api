692fe41485629004b5875c382c8529757c35d6ff
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2024-08-19 19:24:43 -0700

    added team sortable stats w query param

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -1,8 +1,10 @@
 import httpx
+from typing import Optional
 from fastapi import APIRouter, HTTPException, status
 from ..classes import playerstats
 from ..database import get_db_url
 import sqlitecloud
+
 
 router = APIRouter(
   prefix="/stats",
@@ -10,25 +12,46 @@ router = APIRouter(
 )
 
 @router.get("/{player_id}")
-async def get_player_stats(player_id: int):
+async def get_player_stats(player_id: int, sort: Optional[str] = False):
     stats_data = playerstats.PlayerStats(player_id)
 
-    try:
+    if sort == "team":
+      try:
         r = httpx.get(stats_data.url())
         r.raise_for_status()
         html = r.text
         stats_data.parse_stats_html(html)
+
         if stats_data.full_name is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
-    except httpx.HTTPStatusError as e:
+          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
+        team_stats = stats_data.parse_team_stats(html, json=True)
+      except httpx.HTTPStatusError as e:
         # the data url will always return a 200 status code afaik
         raise HTTPException(status_code=e.response.status_code, detail="Player not found")
 
-    return {
+      return {
         "player_id": player_id,
         "full_name": stats_data.full_name,
         "stats_url": stats_data.url(),
-        "stats": stats_data,
+        "team_stats": team_stats,
+      } 
+
+    try:
+      r = httpx.get(stats_data.url())
+      r.raise_for_status()
+      html = r.text
+      stats_data.parse_stats_html(html)
+      if stats_data.full_name is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
+    except httpx.HTTPStatusError as e:
+      # the data url will always return a 200 status code afaik
+      raise HTTPException(status_code=e.response.status_code, detail="Player not found")
+
+    return {
+      "player_id": player_id,
+      "full_name": stats_data.full_name,
+      "stats_url": stats_data.url(),
+      "stats": stats_data,
     }
 
 @router.get("/name/{full_name}")
@@ -38,8 +61,8 @@ async def get_player_stats(full_name: str):
   result = cursor.fetchone()
 
   if result is None:
-      conn.close()
-      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
+    conn.close()
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
   
   conn.close()
   return {
