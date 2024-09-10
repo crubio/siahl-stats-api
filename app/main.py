fc4e58cdbc747915c2118c47519e99f5738571dd
Chris Rubio <crubio@Chriss-MacBook-Pro.local>
2024-09-10 15:35:53 -0700

    updated origin policy for testing

--- app/main.py
+++ app/main.py
@@ -2,8 +2,19 @@ from typing import Union
 from fastapi import FastAPI
 from .routers import stats
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(stats.router)
 
